3d53fc6cc0685f367415b739c4a13cf8ebbf86bd
Mark5795 <markkea95@gmail.com>
2018-12-13 16:03:39 +0100

    Dbconnection af

---
+++ ChapooApplication/DAL/DBconnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ChapooApplication.DAL
+{
+    class DBconnection
+    {
+        public SqlConnection Dbconnection()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["DBConnectionStringSQL"].ConnectionString;
+            SqlConnection sqls = new SqlConnection(connString);
+
+            return sqls;
+        }
+    }
+}
